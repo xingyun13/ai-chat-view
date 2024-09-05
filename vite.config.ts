459989aa4c8c496f0d5e059f49326d30c07a704e
彭帅 <2077796713@qq.com>
2024-09-05 14:33:57 +0800

    初始化版本

---
+++ vite.config.ts
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+//@ts-ignore
+import viteCompression from 'vite-plugin-compression'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', //打包路径
+  plugins: [
+    vue(),
+    // gzip压缩 生产环境生成 .gz 文件
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        additionalData:'@import "@/assets/style/mian.scss";'
+      }
+    }
+  },
+  //启动服务配置
+  server: {
+    // 是否开启 https
+    https: undefined,
+    // 端口号
+    port: 3000,
+    // 监听所有地址
+    host: '0.0.0.0',
+    // 服务启动时是否自动打开浏览器
+    open: true,
+    // 允许跨域
+    cors: true,
+    // 自定义代理规则
+    proxy: {},
+  },
+  // 生产环境打包配置
+  //去除 console debugger
+  build: {
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
+})
+
